9cebad8e60a523f1d8b88a1a72ea19f2d8993784
hyPnOtICDo0g <44530973+hyPnOtICDo0g@users.noreply.github.com>
2021-09-25 22:08:24 +0530

    Several bug fixes & optimizations
    - Improved `/get` command logic
    - rewrote database functions to reduce code duplication
    - better variable names
    - removed INIT_FEEDS; feeds when initiated, will write the latest feed item to the database
    - removed unused imports
    - miscellaneous optimizations & fixes

--- telegramRSSbot.py
+++ telegramRSSbot.py
@@ -1,36 +1,22 @@
 import os
-from pathlib import Path
+import logging
 
 import feedparser
-import logging
 from dotenv import load_dotenv
 
 import psycopg2
 from psycopg2 import Error
 
 from telegram import Bot
+from telegram.error import BadRequest
 from telegram.ext import Updater, CommandHandler, MessageFilter
 
-# Init 
-
 rss_dict = {}
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+load_dotenv('config.env')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-# Prevent unauthorised access to the bot
-
-class OwnerFilter(MessageFilter):
-    def filter(self, message):
-        return bool(message.from_user.id == OWNER_ID)
-
-owner_filter = OwnerFilter()
-
-# Config
-
-load_dotenv('config.env')
-
+# Environment Variables
 def getConfig(name: str):
     return os.environ[name]
 
@@ -46,14 +32,11 @@ except KeyError as e:
     LOGGER.error("One or more env variables missing! Exiting now.")
     exit(1)
 try:
-    INIT_FEEDS = getConfig('INIT_FEEDS')    
-    CUSTOM_MESSAGES = getConfig('CUSTOM_MESSAGES')    
+    CUSTOM_MESSAGES = getConfig('CUSTOM_MESSAGES')
 except:
     pass
 
-
 # Bot Commands
-
 bot = Bot(BOT_TOKEN)
 botcmds = [
         (f'help','To get this message'),
@@ -62,85 +45,91 @@ botcmds = [
         (f'sub','Subscribe to a RSS feed'),
         (f'unsub','Remove a specific subscription'),
         (f'unsuball','Remove all subscriptions')
-    ]  
+    ]
 
-# Postgresql
+# Prevent unauthorised access to the bot
+class OwnerFilter(MessageFilter):
+    def filter(self, message):
+        return bool(message.from_user.id == OWNER_ID)
+owner_filter = OwnerFilter()
 
-def init_postgres():
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-        c = conn.cursor()
-        c.execute("CREATE TABLE rss (name text, link text, last text, title text)")
-        conn.commit()
-        conn.close()
-        LOGGER.info("Database Created.")
-    except psycopg2.errors.DuplicateTable:
-        LOGGER.info("Database already exists.")
+# Database Operations
+class PostgreSQL():
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(DATABASE_URL)
+            self.cur = self.conn.cursor()
+        except psycopg2.DatabaseError as error:
+            LOGGER.error(error)
+
+    def disconnect(self):
+        self.conn.commit()
+        self.cur.close()
+        self.conn.close()
+
+    def init(self):
+        try:
+            self.connect()
+            self.cur.execute("CREATE TABLE rss (name text, link text, last text, title text)")
+            self.disconnect()
+            LOGGER.info("Database Created.")
+        except psycopg2.errors.DuplicateTable:
+            LOGGER.info("Database already exists.")
+            rss_load()
+
+    def load_all(self):
+        self.connect()
+        self.cur.execute("SELECT * FROM rss")
+        rows = self.cur.fetchall()
+        self.disconnect()
+        return rows
+
+    def write(self, name, link, last, title):
+        self.connect()
+        q = [(name), (link), (last), (title)]
+        self.cur.execute("INSERT INTO rss (name, link, last, title) VALUES(%s, %s, %s, %s)", q)
+        self.disconnect()
         rss_load()
 
-def postgres_load_all():
-    conn = psycopg2.connect(DATABASE_URL)
-    c = conn.cursor()
-    c.execute("SELECT * FROM rss")
-    rows = c.fetchall()
-    conn.close()
-    return rows
+    def update(self, last, name, title):
+        self.connect()
+        q = [(last), (title), (name)]
+        self.cur.execute("UPDATE rss SET last=%s, title=%s WHERE name=%s", q)
+        self.disconnect()
 
-def postgres_write(name, link, last, title):
-    conn = psycopg2.connect(DATABASE_URL)
-    c = conn.cursor()
-    q = [(name), (link), (last), (title)]
-    c.execute("INSERT INTO rss (name, link, last, title) VALUES(%s, %s, %s, %s)", q)
-    conn.commit()
-    conn.close()
-    rss_load()
+    def find(self, q):
+        self.connect()
+        # check the database for the latest feed
+        self.cur.execute("SELECT link FROM rss WHERE name = %s", q)
+        feed_url = self.cur.fetchone()
+        self.disconnect()
+        return feed_url
 
-def postgres_update(last, name, title):
-    conn = psycopg2.connect(DATABASE_URL)
-    c = conn.cursor()
-    q = [(last), (title), (name)]
-    c.execute("UPDATE rss SET last=%s, title=%s WHERE name=%s", q)
-    conn.commit()
-    conn.close()
+    def delete(self, q):
+        try:
+            self.connect()
+            self.cur.execute("DELETE FROM rss WHERE name = %s", q)
+            self.disconnect()
+        except psycopg2.errors.UndefinedTable:
+            pass
+        rss_load()
 
-def postgres_find(q):
-    conn = psycopg2.connect(DATABASE_URL)
-    c = conn.cursor()
-    # check the database for the latest feed
-    c.execute("SELECT link FROM rss WHERE name = %s", q)
-    feedurl = c.fetchone()
-    conn.close()
-    return feedurl
+    def deleteall(self):
+        self.connect()
+        # clear database & dictionary
+        self.cur.execute("TRUNCATE TABLE rss")
+        self.disconnect()
+        rss_dict.clear()
+        LOGGER.info('Database deleted.')
+postgres = PostgreSQL()
 
-def postgres_delete(q):
-    conn = psycopg2.connect(DATABASE_URL)
-    c = conn.cursor()
-    try:
-        c.execute("DELETE FROM rss WHERE name = %s", q)
-        conn.commit()
-        conn.close()
-    except psycopg2.errors.UndefinedTable:
-        pass
-    rss_load()
-
-def postgres_deleteall():
-    # clear database & dictionary
-    conn = psycopg2.connect(DATABASE_URL)
-    c = conn.cursor()
-    c.execute("TRUNCATE TABLE rss")
-    conn.commit()
-    conn.close()
-    rss_dict.clear()
-    LOGGER.info('Database deleted.')
-
-# RSS
-
+# RSS Operations
 def rss_load():
     # if the dict is not empty, empty it.
     if bool(rss_dict):
         rss_dict.clear()
 
-    for row in postgres_load_all():
+    for row in postgres.load_all():
         rss_dict[row[0]] = (row[1], row[2], row[3])
 
 def cmd_rss_start(update, context):
@@ -152,7 +141,7 @@ def cmd_rss_start(update, context):
 
 def cmd_rsshelp(update, context):
     help_string=f"""
-<b>Commands:</b> 
+<b>Commands:</b>
 • /help: <i>To get this message</i>
 • /list: <i>List your subscriptions</i>
 • /get Title 10: <i>Force fetch last n item(s)</i>
@@ -163,99 +152,92 @@ def cmd_rsshelp(update, context):
     update.effective_message.reply_text(help_string, parse_mode='HTMl')
 
 def cmd_rss_list(update, context):
-    listfeed = ""
     if bool(rss_dict) is False:
         update.effective_message.reply_text("No subscriptions.")
     else:
+        list_feed = ""
         for title, url_list in rss_dict.items():
-            listfeed +=f"Title: {title}\nFeed: {url_list[0]}\n\n"
-        update.effective_message.reply_text(f"<b>Your subscriptions:</b>\n\n" + listfeed, parse_mode='HTMl')
+            list_feed +=f"Title: {title}\nFeed: {url_list[0]}\n\n"
+        update.effective_message.reply_text(f"<b>Your subscriptions:</b>\n\n" + list_feed, parse_mode='HTMl')
 
 def cmd_get(update, context):
     try:
-        count = context.args[1]
+        count = int(context.args[1])
         q = (context.args[0],)
-        feedurl = postgres_find(q)
-        if  feedurl != None:
-            feedinfo = ""
+        feed_url = postgres.find(q)
+        if feed_url != None and count > 0:
             try:
-                if int(context.args[1]) > 0:
-                    feed_num = int(context.args[1])
-                else:
-                    raise Exception  
-            except (ValueError, Exception):
-                update.effective_message.reply_text("Enter a value > 0.")
-                LOGGER.error("You trolling? Study Math doofus.")
-            else:
+                item_info = ""
                 msg = update.effective_message.reply_text(f"Getting the last <b>{count}</b> item(s), please wait!", parse_mode='HTMl')
-                for num_feeds in range(feed_num):
-                    rss_d = feedparser.parse(feedurl[0])
-                    feedinfo +=f"<b>{rss_d.entries[num_feeds]['title']}</b>\n{rss_d.entries[num_feeds]['link']}\n\n"
-                msg.edit_text(feedinfo, parse_mode='HTMl')    
+                rss_d = feedparser.parse(feed_url[0])
+                for item_num in range(count):
+                    item_info +=f"<b>{rss_d.entries[item_num]['title']}</b>\n{rss_d.entries[item_num]['link']}\n\n"
+                msg.edit_text(item_info, parse_mode='HTMl')
+            except (IndexError, BadRequest):
+                msg.edit_text("Parse depth exceeded. Try again with a lower value.", parse_mode='HTMl')
         else:
-            update.effective_message.reply_text("No such feed found.")
-    except IndexError:
+            update.effective_message.reply_text("Enter a vaild title/value.")
+    except (IndexError, ValueError):
         update.effective_message.reply_text("Please use this format to fetch:\n/get Title 10", parse_mode='HTMl')
 
 def cmd_rss_sub(update, context):
-    addfeed = ""
     # try if there are 2 arguments passed
     try:
         context.args[1]
     except IndexError:
         update.effective_message.reply_text(f"Please use this format to add:\n/sub Title https://www.rss-url.com", parse_mode='HTMl')
     else:
+        sub_feed = ""
         try:
             # try if the url is a valid RSS feed
             rss_d = feedparser.parse(context.args[1])
             rss_d.entries[0]['title']
-            title_rss =  f"<b>{rss_d.feed.title}</b> latest record:\n<b>{rss_d.entries[0]['title']}</b>\n{rss_d.entries[0]['link']}"    
-            postgres_write(context.args[0], context.args[1], str(rss_d.entries[0]['link']), str(rss_d.entries[0]['title']))
-            addfeed = f"<b>Subscribed!</b>\nTitle: {context.args[0]}\nFeed: {context.args[1]}"
-            update.effective_message.reply_text(addfeed, parse_mode='HTMl')
+            title_rss =  f"<b>{rss_d.feed.title}</b> latest record:\n<b>{rss_d.entries[0]['title']}</b>\n{rss_d.entries[0]['link']}"
+            postgres.write(context.args[0], context.args[1], str(rss_d.entries[0]['link']), str(rss_d.entries[0]['title']))
+            sub_feed = f"<b>Subscribed!</b>\nTitle: {context.args[0]}\nFeed: {context.args[1]}"
+            update.effective_message.reply_text(sub_feed, parse_mode='HTMl')
             update.effective_message.reply_text(title_rss, parse_mode='HTMl')
         except IndexError:
             update.effective_message.reply_text("The link doesn't seem to be a RSS feed or it's not supported!")
 
 def cmd_rss_unsub(update, context):
     try:
-        q = (context.args[0],)        
-        postgres_delete(q)
+        q = (context.args[0],)
+        postgres.delete(q)
         update.effective_message.reply_text("If it exists in the database, it'll be removed.")
     except IndexError:
-        update.effective_message.reply_text(f"Please use this format to remove:\n/unsub Title", parse_mode='HTMl')                
+        update.effective_message.reply_text(f"Please use this format to remove:\n/unsub Title", parse_mode='HTMl')
 
 def cmd_rss_unsuball(update, context):
     if rss_dict != {}:
-        postgres_deleteall()
+        postgres.deleteall()
         update.effective_message.reply_text("Deleted all subscriptions.")
     else:
         update.effective_message.reply_text("No subscriptions.")
 
 def init_feeds():
-    if INIT_FEEDS == "True":
-        for name, url_list in rss_dict.items():
-            try:
-                rss_d = feedparser.parse(url_list[0])
-                postgres_update(str(rss_d.entries[0]['link']), name, str(rss_d.entries[0]['title']))
-                LOGGER.info("Feed name: "+ name)
-                LOGGER.info("Latest feed item: "+ rss_d.entries[0]['link'])
-            except IndexError:
-                LOGGER.info(f"There was an error while parsing this feed: {url_list[0]}")
-                continue                    
-        rss_load()
-        LOGGER.info('Initiated feeds.')
-
-def rss_monitor(context):
-    feed_info = ""
     for name, url_list in rss_dict.items():
         try:
-            feed_count = 0
-            feed_titles = []
-            feed_urls = []
+            rss_d = feedparser.parse(url_list[0])
+            postgres.update(str(rss_d.entries[0]['link']), name, str(rss_d.entries[0]['title']))
+            LOGGER.info("Feed name: "+ name)
+            LOGGER.info("Latest feed item: "+ rss_d.entries[0]['link'])
+        except IndexError:
+            LOGGER.info(f"There was an error while parsing this feed: {url_list[0]}")
+            continue
+    rss_load()
+    LOGGER.info('Initiated feeds.')
+
+def rss_monitor(context):
+    for name, url_list in rss_dict.items():
+        try:
+            feed_info = ""
             # check whether the URL & title of the latest item is in the database
             rss_d = feedparser.parse(url_list[0])
             if (url_list[1] != rss_d.entries[0]['link'] and url_list[2] != rss_d.entries[0]['title']):
+                feed_count = 0
+                feed_titles = []
+                feed_urls = []
                 # check until a new item pops up
                 while (url_list[1] != rss_d.entries[feed_count]['link'] and url_list[2] != rss_d.entries[feed_count]['title']):
                     feed_titles.insert(0, rss_d.entries[feed_count]['title'])
@@ -265,7 +247,7 @@ def rss_monitor(context):
                     feed_info = f"{CUSTOM_MESSAGES}\n<b>{feed_titles[x]}</b>\n{feed_urls[x]}"
                     context.bot.send_message(CHAT_ID, feed_info, parse_mode='HTMl')
                 # overwrite the existing item with the latest item
-                postgres_update(str(rss_d.entries[0]['link']), name, str(rss_d.entries[0]['title']))
+                postgres.update(str(rss_d.entries[0]['link']), name, str(rss_d.entries[0]['title']))
         except IndexError:
             LOGGER.info(f"There was an error while parsing this feed: {url_list[0]}")
             continue
@@ -280,17 +262,17 @@ def main():
     updater = Updater(token=BOT_TOKEN, use_context=True)
     job_queue = updater.job_queue
     dp = updater.dispatcher
-    bot.set_my_commands(botcmds)  
+    bot.set_my_commands(botcmds)
 
     dp.add_handler(CommandHandler("start", cmd_rss_start))
-    dp.add_handler(CommandHandler("help", cmd_rsshelp, filters=owner_filter))    
+    dp.add_handler(CommandHandler("help", cmd_rsshelp, filters=owner_filter))
     dp.add_handler(CommandHandler("list", cmd_rss_list, filters=owner_filter))
-    dp.add_handler(CommandHandler("get", cmd_get, filters=owner_filter))     
+    dp.add_handler(CommandHandler("get", cmd_get, filters=owner_filter))
     dp.add_handler(CommandHandler("sub", cmd_rss_sub, filters=owner_filter))
     dp.add_handler(CommandHandler("unsub", cmd_rss_unsub, filters=owner_filter))
     dp.add_handler(CommandHandler("unsuball", cmd_rss_unsuball, filters=owner_filter))
 
-    init_postgres()
+    postgres.init()
     init_feeds()
 
     job_queue.run_repeating(rss_monitor, DELAY)
